22e1334ee65302ba736ee12a26db3e6cfd17ff68
Kuba Groblewski <kkgroblewski@gmail.com>
2024-02-03 21:31:50 +0100

    multiple dives export init; deco model init

--- src/stats.rs
+++ src/stats.rs
@@ -1,7 +1,11 @@
-use std::{error::Error, fs, path::PathBuf, fmt::format, os::macos::raw::stat};
-use crate::parser::{self, UDDFDoc, DiveElem};
+use std::{error::Error, fs, path::PathBuf, fmt::format, os::macos::raw::stat, ops::Div};
+use crate::parser::{self, UDDFDoc, DiveElem, WaypointElem};
+use buehlmann_deco::{step, zhl16c};
+use buehlmann_deco::model::ZHLModel;
+use buehlmann_deco::gas::Gas;
+use buehlmann_deco::step::Step;
 
-pub type Depth = f32;
+pub type Depth = f64;
 pub type Seconds = usize;
 
 #[derive(Debug)]
@@ -42,6 +46,7 @@ pub struct Stats {
 impl Stats {
     pub fn from_file(path: &str) -> Result<Stats, Box<dyn Error>> {
         println!("\nFile: {}", path);
+
         let mut stats = Stats {
             dives_no: 0,
             total_time: 0,
@@ -56,12 +61,7 @@ impl Stats {
             stats.update_with_dive_data(dive_stats);
         }
 
-
         Ok(stats)
-        // --
-        // let dive_stats = Self::handle_file(path);
-        // dbg!(dive_stats);
-        // Ok(stats)
     }
 
     fn extract_dives_from_file(path: &str) -> Result<Vec<DiveElem>, Box<dyn Error>> {
@@ -80,27 +80,16 @@ impl Stats {
 
     fn calc_dive_stats(dive: DiveElem) -> Result<Dive, Box<dyn Error>> {
         let mut dive_stats = Dive::new();
+        let mut deco = zhl16c();
         let dive_data_points = dive.samples.waypoints;
         let mut last_waypoint_time: usize = 0;
         for data_point in dive_data_points {
-            // max depth
-            if data_point.depth > dive_stats.depth_max {
-                dive_stats.update_depth_max(data_point.depth);
-            }
-            // time
-            dive_stats.update_total_time(data_point.dive_time - last_waypoint_time);
-            // decostop
-            match data_point.decostops {
-                Some(decostops) => {
-                    let mandatory_stop_index = decostops
-                        .iter()
-                        .position(|ds| ds.kind == "mandatory");
-                    if let Some(_) = mandatory_stop_index {
-                        dive_stats.update_time_in_deco(data_point.dive_time - last_waypoint_time);
-                    }
-                },
-                None => (),
-            }
+            Self::process_data_point(
+                &mut dive_stats,
+                &data_point,
+                &last_waypoint_time,
+                &mut deco
+            );
             // update last waypoint time
             last_waypoint_time = data_point.dive_time;
         }
@@ -108,32 +97,21 @@ impl Stats {
         Ok(dive_stats)
     }
 
+    fn process_data_point(dive_stats: &mut Dive, data_point: &WaypointElem, last_waypoint_time: &usize, model: &mut ZHLModel) -> () {
+        // max depth
+        if data_point.depth > dive_stats.depth_max {
+            dive_stats.update_depth_max(data_point.depth);
+        }
+        // time
+        let step_time = data_point.dive_time - last_waypoint_time;
+        dive_stats.update_total_time(step_time);
 
+        // buehlmann deco
+        let air = Gas::new(0.21);// tmp
+        &model.step(&data_point.depth, &step_time, &air);
+        println!("depth: {}, ceil: {}", &data_point.depth, &model.ceiling());
 
-    // ---
-
-    // pub fn from_dir(path: &str) -> Result<Stats, Box<dyn Error>> {
-    //     println!("\nDives folder: {}", path);
-    //     let mut stats = Stats {
-    //         dives_no: 0,
-    //         total_time: 0,
-    //         depth_max: 0.0,
-    //         deco_dives_no: 0,
-    //         time_in_deco: 0,
-    //     };
-
-    //     let entries = fs::read_dir(path)?;
-    //     for entry in entries {
-    //         let entry = entry?;
-    //         let entry_path = entry.path();
-    //         if (Self::validate_uddf_target(&entry_path)) {
-    //             let dive_stats = Self::handle_file(entry_path.to_str().unwrap())?;
-    //             stats.update_with_dive_data(dive_stats);
-    //         }
-    //     }
-
-    //     Ok(stats)
-    // }
+    }
 
     fn update_with_dive_data(&mut self, dive_stats: Dive) {
         self.dives_no += 1;
@@ -146,62 +124,6 @@ impl Stats {
             self.deco_dives_no += 1;
         }
     }
-
-    // fn handle_file(path: &str) -> Result<Dive, Box<dyn Error>> {
-    //     println!("Analyzing UDDF: {}", path);
-    //     let test_file = parser::parse_file(path)?;
-    //     let dive_stats = Self::get_dive_stats(test_file)?;
-    //     Ok(dive_stats)
-    // }
-
-    // fn validate_uddf_target(path: &PathBuf) -> bool {
-    //     if !path.is_file() {
-    //         return false;
-    //     }
-    //     let extension = path.extension();
-    //     if extension.is_none() {
-    //         return false;
-    //     }
-    //     let extension = extension.unwrap().to_str().unwrap();
-    //     ["uddf", "UDDF"].contains(&extension)
-    // }
-
-    // fn get_dive_stats(dive: UDDFDoc) -> Result<Dive, Box<dyn Error>> {
-    //     let mut dive_stats = Dive::new();
-    //     let dive_data_points = dive
-    //         .profile_data
-    //         .repetition_group
-    //         .dive
-    //         .samples
-    //         .waypoints;
-
-    //     let mut last_waypoint_time: usize = 0;
-    //     for data_point in dive_data_points {
-    //         // max depth
-    //         if data_point.depth > dive_stats.depth_max {
-    //             dive_stats.update_depth_max(data_point.depth);
-    //         }
-    //         // time
-    //         dive_stats.update_total_time(data_point.dive_time - last_waypoint_time);
-    //         // decostop
-    //         match data_point.decostops {
-    //             Some(decostops) => {
-    //                 let mandatory_stop_index = decostops
-    //                     .iter()
-    //                     .position(|ds| ds.kind == "mandatory");
-    //                 if let Some(_) = mandatory_stop_index {
-    //                     dive_stats.update_time_in_deco(data_point.dive_time - last_waypoint_time);
-    //                 }
-    //             },
-    //             None => (),
-    //         }
-
-    //         // update last waypoint time
-    //         last_waypoint_time = data_point.dive_time;
-    //     }
-
-    //     Ok(dive_stats)
-    // }
 
     pub fn print(&self) -> () {
         println!("\n---------- STATS ----------");
