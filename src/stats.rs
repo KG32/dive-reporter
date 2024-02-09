f125c3353132b4b207371ec24032d05b40fe0026
Kuba Groblewski <kkgroblewski@gmail.com>
2024-02-09 14:03:56 +0100

    use gases based on switchmix

--- src/stats.rs
+++ src/stats.rs
@@ -18,6 +18,7 @@ pub struct Dive {
     total_time: Seconds,
     depth_max: Depth,
     time_in_deco: Seconds,
+    gf_end: f64,
     metadata: DiveMetadata,
 }
 
@@ -27,6 +28,7 @@ impl Dive {
             total_time: 0,
             depth_max: 0.0,
             time_in_deco: 0,
+            gf_end: 0.,
             metadata: DiveMetadata { last_gas: Gas::new(0.21) }
         }
     }
@@ -38,6 +40,9 @@ impl Dive {
     }
     fn update_time_in_deco(&mut self, time: Seconds) {
         self.time_in_deco += time;
+    }
+    fn change_gas(&mut self, gas: Gas) {
+        self.metadata.last_gas = gas;
     }
 }
 
@@ -98,23 +103,37 @@ impl Stats {
     fn calc_dive_stats(&self, dive_data: &DiveElem, gas_mixes: &GasMixesData) -> Result<Dive, Box<dyn Error>> {
         let mut dive = Dive::new();
         let mut model = zhl16c();
-        let dive_data_points = &dive_data.samples.waypoints;
+
+        // calc by data point
         let mut last_waypoint_time: usize = 0;
+        let dive_data_points = &dive_data.samples.waypoints;
         for data_point in dive_data_points {
             self.process_data_point(
                 &mut dive,
                 &mut model,
                 &data_point,
                 &last_waypoint_time,
+                &gas_mixes,
             );
             // update last waypoint time
             last_waypoint_time = data_point.dive_time;
         }
 
+        // end GF
+        let (gf_end, ..) = model.gfs_current();
+        dive.gf_end = gf_end;
+
         Ok(dive)
     }
 
-    fn process_data_point(&self, dive: &mut Dive, model: &mut ZHLModel, data_point: &WaypointElem, last_waypoint_time: &usize) -> () {
+    fn process_data_point(
+        &self,
+        dive: &mut Dive,
+        model: &mut ZHLModel,
+        data_point: &WaypointElem,
+        last_waypoint_time: &usize,
+        gas_mixes: &GasMixesData,
+    ) -> () {
         // max depth
         if data_point.depth > dive.depth_max {
             dive.update_depth_max(data_point.depth);
@@ -125,11 +144,40 @@ impl Stats {
         dive.update_total_time(step_time);
 
         // deco model
-        // @todo real gas used
+        let switchmix = &data_point.switchmix;
+        match switchmix {
+            Some(switchmix) => {
+                let gas_ref = &switchmix.gas_ref;
+                let gas = Self::gas_by_ref(gas_ref, gas_mixes);
+                if gas.is_none() {
+                    panic!("Gas not found");
+                }
+                dive.change_gas(gas.unwrap());
+            },
+            None => ()
+        }
         let gas = &dive.metadata.last_gas;
+        println!("current gas: {:?}", gas);
         model.step(&data_point.depth, &step_time, gas);
 
         // gradient factors
+    }
+
+    fn gas_by_ref(gas_ref: &str, gas_mixes: &GasMixesData) -> Option<Gas> {
+        let mut gas: Option<Gas> = None;
+        match gas_mixes {
+            Some(gas_mixes) => {
+                for mix_definition in gas_mixes {
+                    if mix_definition.id == gas_ref {
+                        gas = Some(Gas::new(mix_definition.o2));
+                    }
+                }
+            },
+            None => {
+                panic!("No gas mixes, can't process switch");
+            }
+        }
+        gas
     }
 
     fn update_with_dive_data(&mut self, dive: Dive) {
