87a8a4d814caa60a1e14119236c4d8413293eba9
Kuba Groblewski <kkgroblewski@gmail.com>
2024-05-20 00:28:52 +0200

    app rendering methods

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use eframe::egui::{self, InnerResponse, Ui};
-use std::future::Future;
+use std::{future::Future, path::PathBuf};
 use std::error::Error;
 use rfd::FileDialog;
 use crate::{dive, stats::{Stats, StatsOutput}};
 
+#[derive(Clone)]
 pub struct App {
     title: String,
     stats: Stats,
@@ -14,14 +15,17 @@ pub struct App {
     state: AppState,
 }
 
+#[derive(Clone)]
 struct AppState {
     error: Option<AppError>
 }
 
+#[derive(Clone)]
 struct AppError {
     text: String,
 }
 
+#[derive(Clone)]
 struct AppConfig {
     path: Option<String>,
     gradient_factors: (u8, u8),
@@ -55,49 +59,22 @@ impl eframe::App for App {
             ui.separator();
 
             // open file btn
-            if ui.button("📂 Open UDDF file").clicked() {
-                let file = FileDialog::new()
-                    .set_directory("/")
-                    .pick_file();
+            self.render_file_btns(ui);
 
-                if let Some(file_path) = file {
-                    // todo err handling
-                    let selected_path = file_path.to_str().unwrap();
-                    self.update_path(selected_path.to_string());
-                    let stats_res = Stats::new().from_path(file_path.to_str().unwrap());
-                    match stats_res {
-                        Ok(stats) => { self.stats = stats },
-                        Err(err) => { self.handle_stats_error(ui, err) }
-                    }
-                }
-            }
             ui.separator();
 
             // stats container
-            let stats = &self.stats;
-            if stats.dives_no > 0 {
-                let depth_max = stats.depth_max.to_string();
-                let gf_surf_max = stats.gf_surf_max.round().to_string();
-                let gf_99_max = stats.gf_99_max.round().to_string();
-                let gf_end_max = stats.gf_end_max.round().to_string();
-
-                ui.vertical(|ui| {
-                    self.render_pair(ui, "Dives:", &stats.dives_no.to_string());
-                    self.render_pair(ui, "Total time:", &Stats::seconds_to_readable(stats.total_time));
-                    self.render_pair(ui, "Max depth", &format!("{depth_max}m"));
-                    self.render_pair(ui, "Deco dives:", &stats.deco_dives_no.to_string());
-                    self.render_pair(ui, "Total time in deco:", &Stats::seconds_to_readable(stats.time_in_deco));
-                    self.render_pair(ui, "Max surface GF:", &format!("{gf_surf_max}%"));
-                    self.render_pair(ui, "Max GF99:", &format!("{gf_99_max}%"));
-                    self.render_pair(ui, "Max end GF:", &format!("{gf_end_max}%"));
-                    self.render_pair(ui, "Time below:", "");
-                    for record in stats.time_below.iter() {
-                        let (depth, time) = record;
-                        ui.indent("", |ui| {
-                            self.render_pair(ui, &format!("-{depth}:"), &Stats::seconds_to_readable(*time));
-                        });
+            match &self.state.error {
+                None => {
+                    let stats = self.stats.clone();
+                    if stats.dives_no > 0 {
+                        self.state.error = None;
+                        self.render_stats(ui, &stats)
                     }
-                });
+                },
+                Some(err) => {
+                    self.render_error(ui, &err);
+                }
             }
         });
     }
@@ -107,6 +84,8 @@ impl App {
     pub fn init(&self) -> Result<(), eframe::Error> {
         let options = eframe::NativeOptions {
             viewport: egui::ViewportBuilder::default().with_inner_size([300.0, 600.0]),
+            follow_system_theme: false,
+            default_theme: eframe::Theme::Dark,
             ..Default::default()
         };
         eframe::run_native(
@@ -118,13 +97,41 @@ impl App {
         )
     }
 
-    fn handle_stats_error(&self, ui: &mut Ui, err: Box<dyn Error>) {
-        println!("Stats error!");
-        println!("{}", err);
+    fn render_file_btns(&mut self, ui: &mut Ui) {
+        if ui.button("📂 Open UDDF file").clicked() {
+            let file = FileDialog::new()
+                .set_directory("/")
+                .pick_file();
+
+            if let Some(file_path) = file {
+                self.run_stats(&file_path);
+            }
+        }
     }
 
-    fn update_path(&mut self, new_path: String) {
-        self.config.path = Some(new_path);
+    fn render_stats(&mut self, ui: &mut Ui, stats: &Stats) {
+        let depth_max = stats.depth_max.to_string();
+        let gf_surf_max = stats.gf_surf_max.round().to_string();
+        let gf_99_max = stats.gf_99_max.round().to_string();
+        let gf_end_max = stats.gf_end_max.round().to_string();
+
+        ui.vertical(|ui| {
+            self.render_pair(ui, "Dives:", &stats.dives_no.to_string());
+            self.render_pair(ui, "Total time:", &Stats::seconds_to_readable(stats.total_time));
+            self.render_pair(ui, "Max depth", &format!("{depth_max}m"));
+            self.render_pair(ui, "Deco dives:", &stats.deco_dives_no.to_string());
+            self.render_pair(ui, "Total time in deco:", &Stats::seconds_to_readable(stats.time_in_deco));
+            self.render_pair(ui, "Max surface GF:", &format!("{gf_surf_max}%"));
+            self.render_pair(ui, "Max GF99:", &format!("{gf_99_max}%"));
+            self.render_pair(ui, "Max end GF:", &format!("{gf_end_max}%"));
+            self.render_pair(ui, "Time below:", "");
+            for record in stats.time_below.iter() {
+                let (depth, time) = record;
+                ui.indent("", |ui| {
+                    self.render_pair(ui, &format!("-{depth}:"), &Stats::seconds_to_readable(*time));
+                });
+            }
+        });
     }
 
     pub fn render_pair(&self, ui: &mut Ui, v1: &str, v2: &str) -> InnerResponse<()> {
@@ -132,5 +139,38 @@ impl App {
             ui.label(v1);
             ui.label(v2);
         })
+    }
+
+    fn render_error(&self, ui: &mut Ui, err: &AppError) {
+        let err_details_text = err.text.to_string();
+
+        ui.vertical(|ui| {
+            ui.heading("Error while reading stats");
+            ui.label(format!("Details: {err_details_text}"));
+        });
+    }
+
+    fn update_path(&mut self, new_path: String) {
+        self.config.path = Some(new_path);
+    }
+
+    fn run_stats(&mut self, file_path: &PathBuf) {
+        let selected_path = file_path.to_str().unwrap();
+        self.update_path(selected_path.to_string());
+        let stats_res = Stats::new().from_path(file_path.to_str().unwrap());
+        match stats_res {
+            Ok(stats) => {
+                if let Some(err) = &self.state.error {
+                    self.state.error = None;
+                }
+                self.stats = stats
+            },
+            Err(err) => {
+                let app_err = AppError {
+                    text: err.to_string()
+                };
+                self.state.error = Some(app_err);
+            }
+        }
     }
 }
