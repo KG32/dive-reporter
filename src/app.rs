c605e49fbb3f8aad4333cde8933b29761ea0088b
Kuba Groblewski <kkgroblewski@gmail.com>
2024-05-19 23:47:54 +0200

    gui render stats init

--- src/app.rs
+++ src/app.rs
@@ -2,6 +2,7 @@
 
 use eframe::egui::{self, InnerResponse, Ui};
 use std::future::Future;
+use std::error::Error;
 use rfd::FileDialog;
 use crate::{dive, stats::{Stats, StatsOutput}};
 
@@ -9,6 +10,21 @@ pub struct App {
     title: String,
     stats: Stats,
     stats_output: StatsOutput,
+    config: AppConfig,
+    state: AppState,
+}
+
+struct AppState {
+    error: Option<AppError>
+}
+
+struct AppError {
+    text: String,
+}
+
+struct AppConfig {
+    path: Option<String>,
+    gradient_factors: (u8, u8),
 }
 
 impl Default for App {
@@ -17,6 +33,13 @@ impl Default for App {
             title: "Dive reporter".to_owned(),
             stats: Stats::new(),
             stats_output: vec![],
+            config: AppConfig {
+                path: None,
+                gradient_factors: (30, 70)
+            },
+            state: AppState {
+                error: None,
+            }
         }
     }
 }
@@ -25,29 +48,55 @@ impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading(&self.title);
-            // a simple button opening the dialog
+            ui.separator();
+
+            // config
+            self.render_pair(ui, "Path:", &self.config.path.clone().unwrap_or("-".to_string()));
+            ui.separator();
+
+            // open file btn
             if ui.button("📂 Open UDDF file").clicked() {
                 let file = FileDialog::new()
-                .set_directory("/")
-                .pick_file();
+                    .set_directory("/")
+                    .pick_file();
 
                 if let Some(file_path) = file {
                     // todo err handling
-                    self.stats = Stats::new().from_path(file_path.to_str().unwrap()).unwrap();
+                    let selected_path = file_path.to_str().unwrap();
+                    self.update_path(selected_path.to_string());
+                    let stats_res = Stats::new().from_path(file_path.to_str().unwrap());
+                    match stats_res {
+                        Ok(stats) => { self.stats = stats },
+                        Err(err) => { self.handle_stats_error(ui, err) }
+                    }
                 }
             }
+            ui.separator();
 
+            // stats container
             let stats = &self.stats;
             if stats.dives_no > 0 {
+                let depth_max = stats.depth_max.to_string();
+                let gf_surf_max = stats.gf_surf_max.round().to_string();
+                let gf_99_max = stats.gf_99_max.round().to_string();
+                let gf_end_max = stats.gf_end_max.round().to_string();
+
                 ui.vertical(|ui| {
                     self.render_pair(ui, "Dives:", &stats.dives_no.to_string());
                     self.render_pair(ui, "Total time:", &Stats::seconds_to_readable(stats.total_time));
-                    self.render_pair(ui, "Max depth", &stats.depth_max.to_string());
+                    self.render_pair(ui, "Max depth", &format!("{depth_max}m"));
                     self.render_pair(ui, "Deco dives:", &stats.deco_dives_no.to_string());
-                    self.render_pair(ui, "Total time in deco:", &&Stats::seconds_to_readable(stats.time_in_deco));
-                    self.render_pair(ui, "Max surface GF:", &stats.gf_surf_max.round().to_string());
-                    self.render_pair(ui, "Max GF99:", &stats.gf_99_max.round().to_string());
-                    self.render_pair(ui, "Max end GF:", &stats.gf_end_max.round().to_string());
+                    self.render_pair(ui, "Total time in deco:", &Stats::seconds_to_readable(stats.time_in_deco));
+                    self.render_pair(ui, "Max surface GF:", &format!("{gf_surf_max}%"));
+                    self.render_pair(ui, "Max GF99:", &format!("{gf_99_max}%"));
+                    self.render_pair(ui, "Max end GF:", &format!("{gf_end_max}%"));
+                    self.render_pair(ui, "Time below:", "");
+                    for record in stats.time_below.iter() {
+                        let (depth, time) = record;
+                        ui.indent("", |ui| {
+                            self.render_pair(ui, &format!("-{depth}:"), &Stats::seconds_to_readable(*time));
+                        });
+                    }
                 });
             }
         });
@@ -67,6 +116,15 @@ impl App {
                 Box::<App>::default()
             }),
         )
+    }
+
+    fn handle_stats_error(&self, ui: &mut Ui, err: Box<dyn Error>) {
+        println!("Stats error!");
+        println!("{}", err);
+    }
+
+    fn update_path(&mut self, new_path: String) {
+        self.config.path = Some(new_path);
     }
 
     pub fn render_pair(&self, ui: &mut Ui, v1: &str, v2: &str) -> InnerResponse<()> {
